9d5adc51f04b32475dc39ff697bbb6480f156dff
tirthrajchauhan <tirthraj456@gmail.com>
2019-02-06 16:13:31 -0500

    Splash Screen added with logo

---
+++ app/src/main/java/com/example/tirthraj/smartscanner/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.tirthraj.smartscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+}
