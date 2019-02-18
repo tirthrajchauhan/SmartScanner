bf96e5b58e7b62e4959f1c1f7c5dac92bf7ebe7d
harshkeshwala <keshwalaharsh@gmail.com>
2019-02-18 16:24:07 -0500

    [Access Camera Functionality Added]

--- app/src/main/java/com/example/tirthraj/smartscanner/MainActivity.java
+++ app/src/main/java/com/example/tirthraj/smartscanner/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.tirthraj.smartscanner;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button openCamera;
+    public static final int RC_CAMERA_PERMISSION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openCamera = (Button) findViewById(R.id.openCamera);
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Checking for camera permission
+                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
+
+                // Condition to take action
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+
+                    //Open camera
+                    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                    startActivityForResult(intent, 0);
+                }
+                else {
+                    //Request camera permission from system
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            RC_CAMERA_PERMISSION);
+                }
+            }
+        });
     }
 }
