3d980398f1671a0d0a5b1e854d4012538c69e3ea
tirthrajchauhan <tirthraj456@gmail.com>
2019-04-09 15:39:28 -0400

    Save QR and barcode generated image to storage and show into photo gallery

--- app/src/main/java/com/example/tirthraj/smartscanner/controller/GenerateBarcodeActivity.java
+++ app/src/main/java/com/example/tirthraj/smartscanner/controller/GenerateBarcodeActivity.java
@@ -1,12 +1,21 @@
 package com.example.tirthraj.smartscanner.controller;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.AppCompatSeekBar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,6 +28,9 @@ import com.example.tirthraj.smartscanner.R;
 
 import com.example.tirthraj.smartscanner.controller.BarcodeFormat;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class GenerateBarcodeActivity extends AppCompatActivity {
@@ -30,8 +42,12 @@ public class GenerateBarcodeActivity extends AppCompatActivity {
     private Spinner bType;
 
     private String barcodeType;
-    private Button generateBarcode;
+    private Button generateBarcode,btnSave;
     private String inputString;
+    public static final int RC_STORAGE_PERMISSION = 1000;
+    BitmapDrawable draw;
+    Bitmap bm;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,6 +60,7 @@ public class GenerateBarcodeActivity extends AppCompatActivity {
         barcodeImage = findViewById(R.id.imageView);
 
         bType = findViewById(R.id.spinner);
+        btnSave = (Button)findViewById(R.id.btn_save);
 
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -70,7 +87,7 @@ public class GenerateBarcodeActivity extends AppCompatActivity {
         generateBarcode = (Button) findViewById(R.id.button);
         generateBarcode.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+                public void onClick(View v) {
 
                 inputString = inputTextEdit.getText().toString();
                 if (TextUtils.isEmpty(inputString))
@@ -102,5 +119,56 @@ public class GenerateBarcodeActivity extends AppCompatActivity {
                 }
             }
         });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int permissionCheck = ContextCompat.checkSelfPermission(GenerateBarcodeActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED ) {
+
+                    draw = (BitmapDrawable) barcodeImage.getDrawable();
+                    bm = draw.getBitmap();
+
+
+
+                    try {
+
+
+                        FileOutputStream outStream = null;
+                        File sdCard = Environment.getExternalStorageDirectory();
+                        File dir = new File(sdCard.getAbsolutePath() + "/barCode");
+                        dir.mkdirs();
+                        String fileName = String.format("%s.jpg", inputString);
+                        File outFile = new File(dir, fileName);
+                        outStream = new FileOutputStream(outFile);
+                        bm.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+                        outStream.flush();
+                        outStream.close();
+                        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                        intent.setData(Uri.fromFile(dir));
+                        sendBroadcast(intent);
+
+//           savePicture(bm, "image_name.jpg");
+                        Toast.makeText(getApplicationContext(), "Image saved...", Toast.LENGTH_SHORT).show();
+                    } catch (FileNotFoundException ex) {
+                        Log.d("exception", "" + ex);
+                    } catch (IOException ex) {
+                        Log.d("exception", "" + ex);
+
+                    }
+//            MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "one" ,"qrcode");
+                }
+
+                else {
+                    //Request camera permission from system
+                    ActivityCompat.requestPermissions(GenerateBarcodeActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            RC_STORAGE_PERMISSION);
+                }
+
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/tirthraj/smartscanner/controller/GenerateQRActivity.java
+++ app/src/main/java/com/example/tirthraj/smartscanner/controller/GenerateQRActivity.java
@@ -1,12 +1,23 @@
 package com.example.tirthraj.smartscanner.controller;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.tirthraj.smartscanner.R;
 import com.google.zxing.BarcodeFormat;
@@ -14,12 +25,24 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class GenerateQRActivity extends AppCompatActivity {
 
-    private Button btngenerate;
+    private Button btngenerate,btnSave;
     private EditText qrtext;
     private ImageView qrcode;
+    BitmapDrawable draw;
+     Bitmap bm;
+    String textqr;
+    public static final int RC_STORAGE_PERMISSION = 1000;
+
 
 
     @Override
@@ -30,10 +53,15 @@ public class GenerateQRActivity extends AppCompatActivity {
         qrtext = findViewById(R.id.qrtext);
         qrcode = findViewById(R.id.qrcode);
 
-    btngenerate.setOnClickListener(new View.OnClickListener() {
+
+        btnSave = (Button)findViewById(R.id.btn_save);
+
+
+
+        btngenerate.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            String textqr = qrtext.getText().toString();
+             textqr = qrtext.getText().toString();
             if(textqr != null && !textqr.isEmpty()){
                try {
                    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
@@ -48,6 +76,81 @@ public class GenerateQRActivity extends AppCompatActivity {
         }
     });
 
-    }
 
+    btnSave.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            int permissionCheck = ContextCompat.checkSelfPermission(GenerateQRActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED && textqr != null && !textqr.isEmpty()) {
+
+                draw = (BitmapDrawable) qrcode.getDrawable();
+                bm = draw.getBitmap();
+
+
+                try {
+
+
+                    FileOutputStream outStream = null;
+                    File sdCard = Environment.getExternalStorageDirectory();
+                    File dir = new File(sdCard.getAbsolutePath() + "/qrCode");
+                    dir.mkdirs();
+                    String fileName = String.format("%s.jpg", textqr);
+                    File outFile = new File(dir, fileName);
+                    outStream = new FileOutputStream(outFile);
+                    bm.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+                    outStream.flush();
+                    outStream.close();
+                    Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                    intent.setData(Uri.fromFile(dir));
+                    sendBroadcast(intent);
+
+//           savePicture(bm, "image_name.jpg");
+                    Toast.makeText(getApplicationContext(), "Image saved...", Toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException ex) {
+                    Log.d("exception", "" + ex);
+                } catch (IOException ex) {
+                    Log.d("exception", "" + ex);
+
+                }
+//            MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "one" ,"qrcode");
+            }
+
+            else {
+                //Request camera permission from system
+                ActivityCompat.requestPermissions(GenerateQRActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        RC_STORAGE_PERMISSION);
+            }
+
+
+        }
+    });
+
+
+
+
+}
+
+//    private void savePicture( Bitmap bm, String imgName)
+//    {
+//        OutputStream fOut = null;
+//        String strDirectory = Environment.getExternalStorageDirectory().toString();
+//
+//        File f = new File(strDirectory, imgName);
+//        try {
+//            fOut = new FileOutputStream(f);
+//
+//            /**Compress image**/
+//            bm.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+//            fOut.flush();
+//            fOut.close();
+//
+//            /**Update image to gallery**/
+//            MediaStore.Images.Media.insertImage(getContentResolver(),
+//                    f.getAbsolutePath(), f.getName(), f.getName());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
